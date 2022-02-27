e43dfe7f5ff9b2d85fd3457a22c8e2fd3e1794b8
Noah Johnson <noah.johnson@loupmail.com>
2022-02-27 09:31:51 -0600

    Finish module

--- sksample.c
+++ sksample.c
@@ -9,6 +9,7 @@
 
 #include "sksample.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -309,7 +310,7 @@ static int op_sample_source(const char *pModule, long line_num) {
   }
   
   /* Check that register is valid */
-  if (status && ((i < 0) || (i >= skvm_bufc))) {
+  if (status && ((i < 0) || (i >= skvm_bufc()))) {
     status = 0;
     fprintf(stderr,
       "%s: [Line %ld] Invalid buffer index!\n",
@@ -331,14 +332,592 @@ static int op_sample_source(const char *pModule, long line_num) {
   return status;
 }
 
-/* @@TODO: */
+/*
+ * [i] [x] [y] [width] [height] sample_source_area -
+ */
+static int op_sample_source_area(const char *pModule, long line_num) {
+  
+  int status = 1;
+  int32_t i = 0;
+  int32_t x = 0;
+  int32_t y = 0;
+  int32_t w = 0;
+  int32_t h = 0;
+  int32_t buf_w = 0;
+  int32_t buf_h = 0;
+  
+  /* Check that at least five parameters */
+  if (stack_count() < 5) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Stack underflow on sample_source_area!\n",
+      pModule, line_num);
+  }
+  
+  /* Check parameter types */
+  if (status) {
+    if ((cell_type(stack_index(4)) != CELLTYPE_INTEGER) ||
+        (cell_type(stack_index(3)) != CELLTYPE_INTEGER) ||
+        (cell_type(stack_index(2)) != CELLTYPE_INTEGER) ||
+        (cell_type(stack_index(1)) != CELLTYPE_INTEGER) ||
+        (cell_type(stack_index(0)) != CELLTYPE_INTEGER)) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Wrong types for sample_source_area!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get parameters */
+  if (status) {
+    i = cell_get_int(stack_index(4));
+    x = cell_get_int(stack_index(3));
+    y = cell_get_int(stack_index(2));
+    w = cell_get_int(stack_index(1));
+    h = cell_get_int(stack_index(0));
+  }
+  
+  /* Check that register is valid */
+  if (status && ((i < 0) || (i >= skvm_bufc()))) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Invalid buffer index!\n",
+      pModule, line_num);
+  }
+  
+  /* Check that width and height are at least one */
+  if (status) {
+    if ((w < 1) || (h < 1)) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Subarea dimensions must be positive!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get dimensions of buffer register */
+  if (status) {
+    skvm_get_dim(i, &buf_w, &buf_h);
+  }
+  
+  /* Check that (x, y) is within buffer register area */
+  if (status) {
+    if ((x < 0) || (x >= buf_w) || (y < 0) || (y >= buf_h)) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Subarea outside of buffer boundaries!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Check that full extent of subarea is within buffer register area */
+  if (status) {
+    if ((x > buf_w - w) || (y > buf_h - h)) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Subarea extent outside of buffer boundaries!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Update state */
+  if (status) {
+    m_src = i;
+    m_src_subarea = 1;
+    m_src_buf_w = buf_w;
+    m_src_buf_h = buf_h;
+    m_src_x = x;
+    m_src_y = y;
+    m_src_w = w;
+    m_src_h = h;
+  }
+  
+  /* Remove parameters from stack */
+  if (status) {
+    stack_pop(5);
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * [i] sample_target -
+ */
+static int op_sample_target(const char *pModule, long line_num) {
+  
+  int status = 1;
+  int32_t i = 0;
+  
+  /* Check that at least one parameter */
+  if (stack_count() < 1) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Stack underflow on sample_target!\n",
+      pModule, line_num);
+  }
+  
+  /* Check parameter types */
+  if (status) {
+    if (cell_type(stack_index(0)) != CELLTYPE_INTEGER) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Wrong types for sample_target!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get parameters */
+  if (status) {
+    i = cell_get_int(stack_index(0));
+  }
+  
+  /* Check that register is valid */
+  if (status && ((i < 0) || (i >= skvm_bufc()))) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Invalid buffer index!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_target = i;
+  }
+  
+  /* Remove parameters from stack */
+  if (status) {
+    stack_pop(1);
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * [m] sample_matrix -
+ */
+static int op_sample_matrix(const char *pModule, long line_num) {
+  
+  int status = 1;
+  int32_t m = 0;
+  
+  /* Check that at least one parameter */
+  if (stack_count() < 1) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Stack underflow on sample_matrix!\n",
+      pModule, line_num);
+  }
+  
+  /* Check parameter types */
+  if (status) {
+    if (cell_type(stack_index(0)) != CELLTYPE_INTEGER) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Wrong types for sample_matrix!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get parameters */
+  if (status) {
+    m = cell_get_int(stack_index(0));
+  }
+  
+  /* Check that register is valid */
+  if (status && ((m < 0) || (m >= skvm_matc()))) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Invalid matrix index!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_matrix = m;
+  }
+  
+  /* Remove parameters from stack */
+  if (status) {
+    stack_pop(1);
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * - sample_mask_none -
+ */
+static int op_sample_mask_none(const char *pModule, long line_num) {
+  
+  /* Ignore parameters */
+  (void) pModule;
+  (void) line_num;
+  
+  /* Update state */
+  m_mask_buf = -1;
+  m_x_boundary = 0.0;
+  m_y_boundary = 0.0;
+  m_right = 0;
+  m_below = 0;
+
+  /* Return successful */
+  return 1;
+}
+
+/*
+ * [x] sample_mask_x -
+ */
+static int op_sample_mask_x(const char *pModule, long line_num) {
+  
+  int status = 1;
+  double v = 0.0;
+  
+  /* Check that at least one parameter */
+  if (stack_count() < 1) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Stack underflow on sample_mask_x!\n",
+      pModule, line_num);
+  }
+  
+  /* Check parameter types */
+  if (status) {
+    if (cell_canfloat(stack_index(0))) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Wrong types for sample_mask_x\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get parameters */
+  if (status) {
+    v = cell_get_float(stack_index(0));
+  }
+  
+  /* Check that raster masking is not in effect */
+  if (status && (m_mask_buf >= 0)) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Can't adjust procedural mask when raster mask!\n",
+      pModule, line_num);
+  }
+  
+  /* Check that parameter value is finite and in range [0.0, 1.0] */
+  if (status && (!isfinite(v))) {
+    status = 0;
+    fprintf(stderr,
+    "%s: [Line %ld] Procedural mask boundary must be in [0.0, 1.0]!\n",
+      pModule, line_num);
+  }
+  if (status && (!((v >= 0.0) && (v <= 1.0)))) {
+    status = 0;
+    fprintf(stderr,
+    "%s: [Line %ld] Procedural mask boundary must be in [0.0, 1.0]!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_x_boundary = v;
+  }
+  
+  /* Remove parameters from stack */
+  if (status) {
+    stack_pop(1);
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * [x] sample_mask_y -
+ */
+static int op_sample_mask_y(const char *pModule, long line_num) {
+  
+  int status = 1;
+  double v = 0.0;
+  
+  /* Check that at least one parameter */
+  if (stack_count() < 1) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Stack underflow on sample_mask_y!\n",
+      pModule, line_num);
+  }
+  
+  /* Check parameter types */
+  if (status) {
+    if (cell_canfloat(stack_index(0))) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Wrong types for sample_mask_y\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get parameters */
+  if (status) {
+    v = cell_get_float(stack_index(0));
+  }
+  
+  /* Check that raster masking is not in effect */
+  if (status && (m_mask_buf >= 0)) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Can't adjust procedural mask when raster mask!\n",
+      pModule, line_num);
+  }
+  
+  /* Check that parameter value is finite and in range [0.0, 1.0] */
+  if (status && (!isfinite(v))) {
+    status = 0;
+    fprintf(stderr,
+    "%s: [Line %ld] Procedural mask boundary must be in [0.0, 1.0]!\n",
+      pModule, line_num);
+  }
+  if (status && (!((v >= 0.0) && (v <= 1.0)))) {
+    status = 0;
+    fprintf(stderr,
+    "%s: [Line %ld] Procedural mask boundary must be in [0.0, 1.0]!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_y_boundary = v;
+  }
+  
+  /* Remove parameters from stack */
+  if (status) {
+    stack_pop(1);
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * - sample_mask_left -
+ */
+static int op_sample_mask_left(const char *pModule, long line_num) {
+  
+  int status = 1;
+  
+  /* Check that raster masking is not in effect */
+  if (status && (m_mask_buf >= 0)) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Can't adjust procedural mask when raster mask!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_right = 0;
+  }
+
+  /* Return status */
+  return status;
+}
+
+/*
+ * - sample_mask_right -
+ */
+static int op_sample_mask_right(const char *pModule, long line_num) {
+  
+  int status = 1;
+  
+  /* Check that raster masking is not in effect */
+  if (status && (m_mask_buf >= 0)) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Can't adjust procedural mask when raster mask!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_right = 1;
+  }
+
+  /* Return status */
+  return status;
+}
+
+/*
+ * - sample_mask_above -
+ */
+static int op_sample_mask_above(const char *pModule, long line_num) {
+  
+  int status = 1;
+  
+  /* Check that raster masking is not in effect */
+  if (status && (m_mask_buf >= 0)) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Can't adjust procedural mask when raster mask!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_below = 0;
+  }
+
+  /* Return status */
+  return status;
+}
+
+/*
+ * - sample_mask_below -
+ */
+static int op_sample_mask_below(const char *pModule, long line_num) {
+  
+  int status = 1;
+  
+  /* Check that raster masking is not in effect */
+  if (status && (m_mask_buf >= 0)) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Can't adjust procedural mask when raster mask!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_below = 1;
+  }
+
+  /* Return status */
+  return status;
+}
+
+/*
+ * [i] sample_mask_raster -
+ */
+static int op_sample_mask_raster(const char *pModule, long line_num) {
+  
+  int status = 1;
+  int32_t i = 0;
+  
+  /* Check that at least one parameter */
+  if (stack_count() < 1) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Stack underflow on sample_mask_raster!\n",
+      pModule, line_num);
+  }
+  
+  /* Check parameter types */
+  if (status) {
+    if (cell_type(stack_index(0)) != CELLTYPE_INTEGER) {
+      status = 0;
+      fprintf(stderr,
+        "%s: [Line %ld] Wrong types for sample_mask_raster!\n",
+        pModule, line_num);
+    }
+  }
+  
+  /* Get parameters */
+  if (status) {
+    i = cell_get_int(stack_index(0));
+  }
+  
+  /* Check that register is valid */
+  if (status && ((i < 0) || (i >= skvm_bufc()))) {
+    status = 0;
+    fprintf(stderr,
+      "%s: [Line %ld] Invalid buffer index!\n",
+      pModule, line_num);
+  }
+  
+  /* Update state */
+  if (status) {
+    m_mask_buf = i;
+  }
+  
+  /* Remove parameters from stack */
+  if (status) {
+    stack_pop(1);
+  }
+  
+  /* Return status */
+  return status;
+}
+
+/*
+ * - sample_nearest -
+ */
+static int op_sample_nearest(const char *pModule, long line_num) {
+  
+  /* Ignore parameters */
+  (void) pModule;
+  (void) line_num;
+  
+  /* Update state */
+  m_alg = SKVM_ALG_NEAREST;
+
+  /* Return successful */
+  return 1;
+}
+
+/*
+ * - sample_bilinear -
+ */
+static int op_sample_bilinear(const char *pModule, long line_num) {
+  
+  /* Ignore parameters */
+  (void) pModule;
+  (void) line_num;
+  
+  /* Update state */
+  m_alg = SKVM_ALG_BILINEAR;
+
+  /* Return successful */
+  return 1;
+}
+
+/*
+ * - sample_bicubic -
+ */
+static int op_sample_bicubic(const char *pModule, long line_num) {
+  
+  /* Ignore parameters */
+  (void) pModule;
+  (void) line_num;
+  
+  /* Update state */
+  m_alg = SKVM_ALG_BICUBIC;
+
+  /* Return successful */
+  return 1;
+}
 
 /*
  * Registration function
  * =====================
  */
 
-void skcore_register(void) {
+void sksample_register(void) {
   register_operator("sample", &op_sample);
   register_operator("sample_source", &op_sample_source);
+  register_operator("sample_source_area", &op_sample_source_area);
+  register_operator("sample_target", &op_sample_target);
+  register_operator("sample_matrix", &op_sample_matrix);
+  register_operator("sample_mask_none", &op_sample_mask_none);
+  register_operator("sample_mask_x", &op_sample_mask_x);
+  register_operator("sample_mask_y", &op_sample_mask_y);
+  register_operator("sample_mask_left", &op_sample_mask_left);
+  register_operator("sample_mask_right", &op_sample_mask_right);
+  register_operator("sample_mask_above", &op_sample_mask_above);
+  register_operator("sample_mask_below", &op_sample_mask_below);
+  register_operator("sample_mask_raster", &op_sample_mask_raster);
+  register_operator("sample_nearest", &op_sample_nearest);
+  register_operator("sample_bilinear", &op_sample_bilinear);
+  register_operator("sample_bicubic", &op_sample_bilinear);
 }
